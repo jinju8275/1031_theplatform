910e7a666ef7b07eff32ab7420c251f974f1bdbd
jinju8275 <62647121+jinju8275@users.noreply.github.com>
2020-10-31 23:14:37 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+	$(".window").on('mouseenter',function() {
+		$(".bg1").css("background-color", "red");
+		$(".bg1").css('border', '1px solid white');
+		$(".bg2").css("background-color", "red");
+		$(".bg2").css('border', '1px solid white');
+		$(".bg3").css("background-color", "red");
+		$(".bg3").css('border', '1px solid white');
+		$(".bg4").css("background-color", "red");
+		$(".bg4").css('border', '1px solid white');
+		$(".stage").css("background-color", "red");
+		$(".stage").css('border', '1px solid white');
+		$(".window").css("background-color", "white");
+		$(".window").css('border', '1px solid white');
+		$(".bed").css("background-color", "red");
+		$(".bed").css('border', '1px solid white');
+		$(".main-stage").css("background-color", "red");
+		$(".main-stage").css('border', '1px solid white');
+
+		$(".line").css("background-color", "white");
+		$(".line2").css("background-color", "white");
+		$(".main-text").css("color", "white");
+		$(".netflix").css("color", "white");
+		$(".flim").css("color", "white");
+		$(".net").css("color", "white");
+
+		$(".img1").attr("src", "img/img11.png");
+		$(".img2").attr("src", "img/img22.png");
+		$(".img3").attr("src", "img/img33.png");
+		$(".img4").attr("src", "img/img44.png");
+		$(".img5").attr("src", "img/img55.png");
+		$(".img6").attr("src", "img/img66.png");
+		$(".img7").attr("src", "img/img77.png");
+		$(".img8").attr("src", "img/img88.png");
+		$(".img9").attr("src", "img/img99.png");
+
+	})
+
+	$(".window").on('mouseleave',function() {
+		$(".bg1").css("background-color", "#131415");
+		$(".bg1").css('border', '1px solid #74c5b8');
+		$(".bg2").css("background-color", "#131415");
+		$(".bg2").css('border', '1px solid #74c5b8');
+		$(".bg3").css("background-color", "#131415");
+		$(".bg3").css('border', '1px solid #74c5b8');
+		$(".bg4").css("background-color", "#131415");
+		$(".bg4").css('border', '1px solid #74c5b8');
+		$(".stage").css("background-color", "#131415");
+		$(".stage").css('border', '1px solid #74c5b8');
+		$(".window").css("background-color", "#131415");
+		$(".window").css('border', '1px solid #74c5b8');
+		$(".bed").css("background-color", "#131415");
+		$(".bed").css('border', '1px solid #74c5b8');
+		$(".main-stage").css("background-color", "#131415");
+		$(".main-stage").css('border', '1px solid #74c5b8');
+
+		$(".line").css("background-color", "#74c5b8");
+		$(".line2").css("background-color", "#74c5b8");
+		$(".main-text").css("color", "#74c5b8");
+		$(".netflix").css("color", "#74c5b8");
+		$(".flim").css("color", "#74c5b8");
+		$(".net").css("color", "red");
+
+		$(".img1").attr("src", "img/img1.png");
+		$(".img2").attr("src", "img/img2.png");
+		$(".img3").attr("src", "img/img3.png");
+		$(".img4").attr("src", "img/img4.png");
+		$(".img5").attr("src", "img/img5.png");
+		$(".img6").attr("src", "img/img6.png");
+		$(".img7").attr("src", "img/img7.png");
+		$(".img8").attr("src", "img/img8.png");
+		$(".img9").attr("src", "img/img9.png");
+
+	})
+	
+})
+
+
